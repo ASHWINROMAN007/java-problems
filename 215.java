914c4507f742ea56e4a0abc09e9c46b2181cd224
ashwin <ashwinkrishnan2005@gmail.com>
2024-09-20 20:12:36 +0530

    update daily

---
+++ 215.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        for(int num : nums)
+        {
+            min.add(num);
+            if(min.size()>k)
+            {
+                min.poll();
+            }
+        }
+        return min.peek();
+
+
+    }
+}
