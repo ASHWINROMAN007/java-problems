f9c47ea3c72107837641007122bfe8bf483eef17
ashwin <ashwinkrishnan2005@gmail.com>
2024-11-03 19:07:27 +0530

    take

---
+++ Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+    
+        String S = s + s;
+        return S.contains(goal);
+    }
+}
