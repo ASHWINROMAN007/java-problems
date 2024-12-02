f8d514dbbe487b9db5d37f383403a9dc704bcde7
ashwin <ashwinkrishnan2005@gmail.com>
2024-12-02 15:12:16 +0530

    take

---
+++ 1455.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] w = sentence.split(" ");
+        int n = w.length;
+        for(int i =0;i<n;i++)
+        {
+            if(w[i].startsWith(searchWord))
+            {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
