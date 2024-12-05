145255f18614233fd54bccefcaeabfe7b750b3ed
ashwin <ashwinkrishnan2005@gmail.com>
2024-12-05 20:27:07 +0530

    do

--- array7.java
+++ array7.java
@@ -2,13 +2,25 @@ class Solution {
     public int binarysearch(int[] arr, int k) {
         // Code Here
         int n = arr.length;
-        for(int i =0;i<n;i++)
-        {
-            if(arr[i]==k)
+        int l =0;
+        int r=n-1;
+        int result=-1;
+        while(l<=r){
+        int m = l+(r-l)/2;
+            if(arr[m]==k)
             {
-                return i;
+                result=m;
+                r=m-1;
+            }
+            else if(arr[m]<k)
+            {
+                l=m+1;
+            }
+            else
+            {
+                r=m-1;
             }
         }
-        return -1;
+        return result;
     }
 }
