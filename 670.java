337c65e969219203caede04c4a380d9d28c7e946
ashwin <ashwinkrishnan2005@gmail.com>
2025-04-09 18:15:08 +0530

    done

--- 670.java
+++ 670.java
@@ -3,14 +3,10 @@ class Solution {
         // Convert number to string for digit manipulation
         char[] numArr = Integer.toString(num).toCharArray();
         int n = numArr.length;
-
-        // Track the last occurrence of each digit (0-9)
         int[] last = new int[10];
         for (int i = 0; i < n; i++) {
             last[numArr[i] - '0'] = i;
         }
-
-        // Traverse the number from left to right
         for (int i = 0; i < n; i++) {
             // Check if we can find a larger digit to swap
             for (int d = 9; d > numArr[i] - '0'; d--) {
