577364fd9b7fab2bf60d972fd3c308fb8972764f
ashwin <ashwinkrishnan2005@gmail.com>
2025-04-15 18:12:01 +0530

    checking squared value exits

---
+++ 1346.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer,Integer> m = new HashMap<>();
+        for(int i =0;i<n;i++)
+        {
+            if(m.containsKey(arr[i]*2))
+            {
+                return true;
+            }
+            else if(arr[i]%2==0 && m.containsKey(arr[i]/2))
+            {
+                return true;
+            }
+            m.put(arr[i],m.getOrDefault(arr[i],0)+1);
+        }
+        return false;
+    }
+}
