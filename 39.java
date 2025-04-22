203ca3fd012c25f5a41ac608c579320ae0bef23f
ashwin <ashwinkrishnan2005@gmail.com>
2025-04-22 16:13:59 +0530

    using recuresio checking

---
+++ 39.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void findCombination(int in,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds)
+    {
+        if(in==arr.length)
+        {
+            if(target==0)
+            {
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        if(arr[in]<=target)
+        {
+            ds.add(arr[in]);
+            findCombination(in,arr,target-arr[in],ans,ds);
+            ds.remove(ds.size()-1);
+
+        }
+        findCombination(in+1,arr,target,ans,ds);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombination(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+}
