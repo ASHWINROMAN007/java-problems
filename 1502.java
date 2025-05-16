df643caae5c035a2bc692777ea6a1b0f80953010
ashwin <ashwinkrishnan2005@gmail.com>
2025-05-16 21:16:49 +0530

    can make arithmetic progression

--- 1502.java
+++ 1502.java
@@ -5,7 +5,7 @@ class Solution {
         {
             if(Math.abs(arr[i]-arr[i-1])!=Math.abs(arr[i]-arr[i+1]))
             {
-                return false;
+                 return false;
             }
         }
         return true;
