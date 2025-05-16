9c1271b2a80349c6a84a6d3456c322e9d3e42365
ashwin <ashwinkrishnan2005@gmail.com>
2025-05-16 21:15:01 +0530

    is monotonic

---
+++ 896.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+       boolean in = true,de=true;
+       for(int i = 1;i<nums.length;i++)
+       {
+        if(nums[i]>nums[i-1]) de=false;
+        if(nums[i]<nums[i-1]) in=false;
+       }
+       return in||de;
+    }
+}
