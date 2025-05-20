73fbe9d57201ccef30e6cfb1660b3b4fe1c86aa4
ashwin <ashwinkrishnan2005@gmail.com>
2025-05-20 11:34:02 +0530

    selection sort

---
+++ selectsor.java
@@ -0,0 +1,17 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        int n = arr.length;
+        for(int i =0;i<=n-2;i++)
+        {
+            int min = i;
+            for(int j = i;j<=n-1;j++)
+            {
+                if(arr[j]<arr[min]) min=j;
+            }
+            int t = arr[min];
+            arr[min]=arr[i];
+            arr[i]=t;
+        }
+    }
+}
