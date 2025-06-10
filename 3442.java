be11934a83bac4cab3cd9f29acb06d93fe3bc894
ashwin <ashwinkrishnan2005@gmail.com>
2025-06-10 08:41:54 +0530

    maxscore leetcode

---
+++ 3442.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxDifference(String s) {
+        int[] f = new int[26];
+        for(char c : s.toCharArray())
+        {
+            f[c-'a']++;
+        }
+        int maxod = Integer.MIN_VALUE;
+        int minev = Integer.MAX_VALUE;
+        for(int a : f)
+        {
+            if(a>0)
+            {
+                if(a%2==1)
+                {
+                    maxod=Math.max(a,maxod);
+                }
+                else
+                {
+                    minev=Math.min(a,minev);
+                }
+            }
+        }
+        return maxod-minev;
+    }
+}
