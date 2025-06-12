45e1f55490bfa755ec5c2ba365e3e5a70f0e26cc
ashwin <ashwinkrishnan2005@gmail.com>
2025-06-12 19:19:04 +0530

    optimized circulararray concept

--- 3423.java
+++ 3423.java
@@ -4,19 +4,7 @@ class Solution {
         int max=Integer.MIN_VALUE;
         for(int i = 0;i<n;i++)
         {
-            int dif=0;
-            if(i==n-1)
-            {
-                 dif = Math.abs(nums[i]-nums[0]);
-            }
-            else
-            {
-             dif = Math.abs(nums[i]-nums[i+1]);
-            }
-            if(dif>max)
-            {
-                max=dif;
-            }
+            max = Math.max(max,Math.abs(nums[i]-nums[(i+1)%n]));
         }
         return max;
     }
