f27ba272d0490cc20d02041c6d9652168ac1a285
ashwin <ashwinkrishnan2005@gmail.com>
2025-06-26 07:11:44 +0530

    wiggle sort 2

---
+++ 324.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        int mid = (n - 1) / 2;
+        int end = n - 1;
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] = (i % 2 == 0) ? sorted[mid--] : sorted[end--];
+        }
+    }
+}
