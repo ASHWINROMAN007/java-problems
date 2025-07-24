61f9fe15ea636799abfd495b5e9651c7e60c7068
ashwin <ashwinkrishnan2005@gmail.com>
2025-07-24 09:57:18 +0530

    character replacement

---
+++ characterreplace.java
@@ -0,0 +1,30 @@
+class Solution {
+    static int characterReplacement(String s, int k) {
+        // codinte here
+        int n = s.length();
+        int[] m = new int[26];
+        int l = 0;
+        int r=  0;
+        int max = 0;
+        int maxf=0;
+        while(r<n)
+        {
+            m[s.charAt(r)-'A']++;
+            maxf=Math.max(maxf,m[s.charAt(r)-'A']);
+            if((r-l+1)-maxf>k)
+            {
+               m[s.charAt(l)-'A']--;
+               l++;
+            }
+            if((r-l+1)-maxf<=k)
+            {
+                max=Math.max(max,r-l+1);
+            }
+            r++;
+            
+        }
+        return max;
+        
+        
+    }
+}
