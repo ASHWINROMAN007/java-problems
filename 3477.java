8955729d6c27c94b3e0392e5c4159dcf5bbf5d44
ashwin <ashwinkrishnan2005@gmail.com>
2025-08-05 22:16:56 +0530

    numofunplaced food

---
+++ 3477.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[baskets.length];
+        int unplaced = 0;
+
+        for (int fruit : fruits) {
+            boolean placed = false;
+            for (int i = 0; i < baskets.length; i++) {
+                if (!used[i] && baskets[i] >= fruit) {
+                    used[i] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
+    }
+}
