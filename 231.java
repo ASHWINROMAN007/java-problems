f0cccdbdbf7bd7945d94acd87717b276a14207c9
ashwin <ashwinkrishnan2005@gmail.com>
2025-08-09 13:41:40 +0530

    is power of two (bit)

---
+++ 231.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<1) return false;
+        while(n>1){
+            if((n&1)==1) return false;
+            n=n>>1;
+        }
+        return true;
+    }
+}
