b331843e1f7a698553ab5fc676464000535e12d1
ashwin <ashwinkrishnan2005@gmail.com>
2025-08-22 06:06:58 +0530

    minimum area

---
+++ 3195.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int x1=n;int x2 = -1;
+        int y1 = m; int y2 =-1;
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0 ; j< m;j++)
+            {
+                if(grid[i][j]==0) continue;
+                x1 = Math.min(x1,i);
+                x2 = Math.max(x2,i);
+                y1 = Math.min(y1,j);
+                y2 = Math.max(y2,j);
+            }
+        }
+        return (x2-x1+1)*(y2-y1+1);
+    }
+}
