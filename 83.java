5635166f03c58d875d9e5124d4b4ba8ff5cd6169
ashwin <ashwinkrishnan2005@gmail.com>
2025-10-06 21:54:48 +0530

    delete duplicates

---
+++ 83.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode re = head;
+        while(re!=null&&re.next!=null)
+        {
+            if(re.val==re.next.val)
+            {
+              re.next = re.next.next;
+            }
+            else{
+            re = re.next;
+            }
+        }
+        return head;
+    }
+}
