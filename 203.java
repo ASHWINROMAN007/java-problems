5d81fec888b9bfbf6680b8f8c7f7227f702e06d8
ashwin <ashwinkrishnan2005@gmail.com>
2025-10-08 21:21:31 +0530

    remove elements

---
+++ 203.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode c = head;
+        while(c!=null&&c.next!=null)
+        {
+            if(c.next.val == val)
+            {
+                c.next = c.next.next;
+            }
+            else{
+            c=c.next;
+            }
+        } if (head!=null&&head.val==val) head=head.next;
+            return head;
+    }
+}
